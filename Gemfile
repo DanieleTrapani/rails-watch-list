61ac4046aefffefa59c5f0058307de0dbcca37a6
Daniele Trapani <daniele.trapani96@gmail.com>
2023-08-17 12:02:31 +0200

    models added

--- Gemfile
+++ Gemfile
@@ -67,6 +67,7 @@ end
 gem 'rspec-rails', group: [ :test ]
 gem 'rails-controller-testing', group: [ :test ]
 
+gem 'faker'
 gem "bootstrap", "~> 5.2"
 gem "autoprefixer-rails"
 gem "font-awesome-sass", "~> 6.1"
---
+++ app/models/bookmark.rb
@@ -0,0 +1,8 @@
+class Bookmark < ApplicationRecord
+  validates :comment, length: { minimum: 6 }
+  validates :movie_id, :list_id, presence: true
+  validates :movie_id, uniqueness: { scope: :list_id }
+
+  belongs_to :movie
+  belongs_to :list
+end
